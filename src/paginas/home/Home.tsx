6eb1e3cb546b74758cb095b276f806b794d7ea96
unknown <raquelresende_@outlook.com>
2022-11-23 12:36:03 -0300

    att second

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css"
+
+function Home(){
+    return (
+        <>
+        <h1 className="titulo">Home</h1>
+      <div className="containe">
+         <img className="img" src="http://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png" alt="imagem da home"/>
+       </div>
+         </>
+    );
+}
+export default Home
